0255aba3cdf87bbf1ab3298d4f87c7d2af00a491
andreneves <andr@andr.com.br>
2021-03-05 22:18:43 -0300

    up

--- routes/web.php
+++ routes/web.php
@@ -19,18 +19,24 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+//https://laravel.com/docs/8.x/controllers#actions-handled-by-resource-controller
 
 Route::get('/produto', [ProdutoController::class, 'index'])->name('produto.index');
 
-Route::get('/produto/{id}', [ProdutoController::class, 'show'])->name('produto.show');
 
 /*
-Route::get('produtocreate', function () {
+
+Route::get('/produto', function () {
     dd('ENTROU');
 });
 */
 
-Route::get('/produtocreate', [ProdutoController::class], 'create')->name('produto.create');
 
-Route::post('/produto/create', [ProdutoController::class], 'store')->name('produto.store');
+
+Route::get('/produto/{id}', [ProdutoController::class, 'show'])->name('produto.show');
+
+
+Route::get('/produto/create', [ProdutoController::class, 'create'])->name('produto.create');
+
+Route::post('/produto', [ProdutoController::class, 'store'])->name('produto.store');
 
