ef671c7800197e8b92e746cec7b7f5adcc48af0b
Ediberto Soares <47989722+xS0ares@users.noreply.github.com>
2021-03-11 20:45:53 -0300

    20:45

--- resources/views/produto/create.blade.php
+++ resources/views/produto/create.blade.php
@@ -19,35 +19,28 @@
                 height: 100vh;
                 margin: 0;
             }
-
             .full-height {
                 height: 100vh;
             }
-
             .flex-center {
                 align-items: center;
                 display: flex;
                 justify-content: center;
             }
-
             .position-ref {
                 position: relative;
             }
-
             .top-right {
                 position: absolute;
                 right: 10px;
                 top: 18px;
             }
-
             .content {
                 text-align: center;
             }
-
             .title {
                 font-size: 84px;
             }
-
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -57,7 +50,6 @@
                 text-decoration: none;
                 text-transform: uppercase;
             }
-
             .m-b-md {
                 margin-bottom: 30px;
             }
@@ -70,12 +62,24 @@
                 <div class="title m-b-md">
                     CREATE
                 </div>
-
+<!--
                 <form method="POST" action="/produto/create">
                     @csrf
                     <input id="nome" type="text" name="nome">
                     <input id="descricao" type="text" name="descricao">
                 </form>
+            -->
+            {{ Form::open(array('url' => '/produto/create')) }}
+
+            {{ Form::label('nome', 'Nome') }}
+            {{ Form::text('nome') }}
+            <br>
+            {{ Form::label('descricao', 'Descrição') }}
+            {{ Form::text('descricao') }}
+            <br>
+            {{ Form::submit('Criar') }}
+
+            {{ Form::close() }}
 
             </div>
         </div>
